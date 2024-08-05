ddad3d419db6c41d9cf458ec0bed88a2dea7122c
unknown <kayv@hotmail.fr>
2024-08-05 06:18:36 +0200

    Update store.js

--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,36 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState ={
+    user:{
+        id: null,
+        name: '',
+    },
+    token: null,
     playlist: []
-    
 }
+
+const musicSlice = createSlice({
+    name: 'music',
+    initialState,
+    reducers: {
+        createAccount:(state, action) => {
+            state.user.id = action.payload.id
+            state.user.name = action.payload.name
+        },
+        setToken:(state, action ) =>  {
+            state.token = action.payload.token
+        }, 
+        createPlaylist: (state, action) => {
+            state.playlist = action.payload.playlist
+        }
+    }
+})
+
+export const { createAccount, setToken, createPlaylist } = musicSlice.actions;
+
+export const store = configureStore({
+	reducer: { music: musicSlice.reducer},
+    devTools: true
+});
+
+export default store
